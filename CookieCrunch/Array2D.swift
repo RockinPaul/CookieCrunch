1a31694cc605d90d33a0e240f4c00f5bec519354
Pavel Zarudnev <rockinpaulz@gmail.com>
2016-04-02 13:23:58 +0300

    Array2D struct commented.

--- CookieCrunch/Array2D.swift
+++ CookieCrunch/Array2D.swift
@@ -6,18 +6,19 @@
 //  Copyright © 2016 Pavel Zarudnev. All rights reserved.
 //
 
-struct Array2D<T> {
-    let columns: Int
-    let rows: Int
-    private var array: Array<T?>
+struct Array2D<T> { // Мы будем харнить тут Cookies и Tiles. <T> - любой тип
     
-    init(columns: Int, rows: Int) {
+    let columns: Int // Столбец
+    let rows: Int   // Строка
+    private var array: Array<T?> // Приватная переменная. <T?> обуславливает, что значение может быть nil
+    
+    init(columns: Int, rows: Int) { // Инициализация массива
         self.columns = columns
         self.rows = rows
         array = Array<T?>(count: rows*columns, repeatedValue: nil)
     }
     
-    subscript(column: Int, row: Int) -> T? {
+    subscript(column: Int, row: Int) -> T? { // Представляем одномерный массив как двумерный
         get {
             return array[row*columns + column]
         }
