f29d48c013dedf1fe76b76bd80547368910887db
Pavel Zarudnev <rockinpaulz@gmail.com>
2016-04-02 12:59:54 +0300

    Cookie class commented.

--- CookieCrunch/Cookie.swift
+++ CookieCrunch/Cookie.swift
@@ -9,6 +9,7 @@
 import SpriteKit
 
 enum CookieType: Int {
+    // Перечисление имеет тип Int, поэтому мы можем указать лишь первое значение (Unknown)
     case Unknown = 0, Croissant, Cupcake, Danish, Donut, Macaroon, SugarCookie
     
     var spriteName: String {
@@ -20,23 +21,23 @@ enum CookieType: Int {
             "Macaroon",
             "SugarCookie"]
         
-        return spriteNames[rawValue - 1]
+        return spriteNames[rawValue - 1] // Первому элементу в списке спрайтов соответствует нулевой в массиве
     }
     
     var highlightedSpriteName: String {
         return spriteName + "-Highlighted"
     }
     
-    static func random() -> CookieType {
+    static func random() -> CookieType { // Выбор случайного элемента
         return CookieType(rawValue: Int(arc4random_uniform(6)) + 1)!
     }
     
-    var description: String {
+    var description: String { // Описание элемента.
         return spriteName
     }
 }
 
-func == (lhs: Cookie, rhs: Cookie) -> Bool {
+func == (lhs: Cookie, rhs: Cookie) -> Bool { // Перегружаем оператор равенства
     return lhs.column == rhs.column && lhs.row == rhs.row
 }
 
@@ -44,19 +45,19 @@ class Cookie: CustomStringConvertible, Hashable {
     var column: Int
     var row: Int
     let cookieType: CookieType
-    var sprite: SKSpriteNode?
+    var sprite: SKSpriteNode? // Элемент может и не иметь спрайта
     
-    init(column: Int, row: Int, cookieType: CookieType) {
+    init(column: Int, row: Int, cookieType: CookieType) { // Инициализатор принимает координаты печеньки на поле и тип
         self.column = column
         self.row = row
         self.cookieType = cookieType
     }
     
-    var description: String {
+    var description: String { // Описание
         return "type:\(cookieType) square:(\(column),\(row))"
     }
     
-    var hashValue: Int {
+    var hashValue: Int { // Хэш-значение. Именно при реализации протокола Hashable мы должны перегружать оператор равенства
         return row*10 + column
     }
 }
--- CookieCrunch/GameScene.swift
+++ CookieCrunch/GameScene.swift
@@ -10,15 +10,15 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    var level: Level!
+    var level: Level! // Текущий уровень
     
-    let TileWidth: CGFloat = 32.0
-    let TileHeight: CGFloat = 36.0
+    let TileWidth: CGFloat = 32.0 // Ширина плитки
+    let TileHeight: CGFloat = 36.0 // Высота плитки
     
-    let gameLayer = SKNode()
-    let cookiesLayer = SKNode()
+    let gameLayer = SKNode() // Основной слой игры, на нем представлены все объекты
+    let cookiesLayer = SKNode() // Слой печенья, единственного интерактивного объекта на игровом поле
     
-    let tilesLayer = SKNode()
+    let tilesLayer = SKNode() // Слой плитки. Он отображается как фон, на котором расположена печенька
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder) is not used in this app")
@@ -27,40 +27,43 @@ class GameScene: SKScene {
     override init(size: CGSize) {
         super.init(size: size)
         
-        anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        anchorPoint = CGPoint(x: 0.5, y: 0.5) // Определяет координаты начальной позиции для дочерних элементов
         
         let background = SKSpriteNode(imageNamed: "Background")
-        background.size = self.size;
+        background.size = self.size // Задаем фону к размеру экрана
         addChild(background)
         
-        addChild(gameLayer)
+        addChild(gameLayer) // Добавляем основной слой игры
         
-        let layerPosition = CGPoint(
+        let layerPosition = CGPoint( // Задаем позицию игрового поля
             x: -TileWidth * CGFloat(NumColumns) / 2,
             y: -TileHeight * CGFloat(NumRows) / 2)
         
         tilesLayer.position = layerPosition
-        gameLayer.addChild(tilesLayer)
+        gameLayer.addChild(tilesLayer) // Добавляем игровое поле
         
-        cookiesLayer.position = layerPosition
-        gameLayer.addChild(cookiesLayer)
+        cookiesLayer.position = layerPosition // Позиция печенья аналогична игровому полю
+        gameLayer.addChild(cookiesLayer) // Добаляем поле печенья
     }
     
+    // Добавляем изображение печенья его фактическому месторасположению
     func addSpritesForCookies(cookies: Set<Cookie>) {
-        for cookie in cookies {
-            let sprite = SKSpriteNode(imageNamed: cookie.cookieType.spriteName)
-            sprite.position = pointForColumn(cookie.column, row:cookie.row)
-            cookiesLayer.addChild(sprite)
-            cookie.sprite = sprite
+        for cookie in cookies { // Цикл проходит через множество из печенья
+            let sprite = SKSpriteNode(imageNamed: cookie.cookieType.spriteName) // spriteName соответствует имени файла
+            sprite.position = pointForColumn(cookie.column, row:cookie.row) // Получаем позицию для спрайта
+            cookiesLayer.addChild(sprite) // Добавляем спрайт на поле печенья
+            cookie.sprite = sprite // Текущему печенью присваиваем значения спрайта (до этого оно у него могло быть nil)
         }
     }
     
+    // Позиция на экране, соответствующая положению клетки поля
     func pointForColumn(column: Int, row: Int) -> CGPoint {
         return CGPoint(
             x: CGFloat(column)*TileWidth + TileWidth/2,
             y: CGFloat(row)*TileHeight + TileHeight/2)
     }
     
+    // Добавляем поле, которое будет фоном для печенья
     func addTiles() {
         for row in 0..<NumRows {
             for column in 0..<NumColumns {
