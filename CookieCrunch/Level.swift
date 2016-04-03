d5c5c9a963ac64fe08edd031f5693d12e452962b
Pavel Zarudnev <rockinpaulz@gmail.com>
2016-04-03 19:52:21 +0300

    createInitialCookies method fixed.

--- CookieCrunch/Level.swift
+++ CookieCrunch/Level.swift
@@ -49,8 +49,18 @@ class Level {
         for row in 0..<NumRows {
             for column in 0..<NumColumns {
                 if tiles[column, row] != nil {
-                    let cookieType = CookieType.random()
+                    
+                    var cookieType: CookieType
+                    repeat {
+                        cookieType = CookieType.random()
+                    } while (column >= 2 &&
+                            cookies[column - 1, row]?.cookieType == cookieType &&
+                            cookies[column - 2, row]?.cookieType == cookieType)
+                            || (row >= 2 &&
+                                cookies[column, row - 1]?.cookieType == cookieType &&
+                                cookies[column, row - 2]?.cookieType == cookieType)
                     let cookie = Cookie(column: column, row: row, cookieType: cookieType)
+                    
                     cookies[column, row] = cookie
                     set.insert(cookie)
                 }
